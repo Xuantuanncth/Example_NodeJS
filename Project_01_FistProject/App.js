7cc941a1bf40448f6e9b50600ec7ba51d2ab4f8f
XUANTUAN\Xuan Tuan <xuantuanncth1@gmail.com>
2021-09-05 08:53:19 +0700

    Update to fist porject

---
+++ Project_01_FistProject/App.js
@@ -0,0 +1,6 @@
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNote = require("./Note");
+
+console.log(yargs.argv)
---
+++ Project_01_FistProject/Note.js
@@ -0,0 +1,13 @@
+const getNote = function() {
+    return "Your note ..."
+}
+
+const loadNotes = () => {
+
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+}
+
+module.exports = getNote
