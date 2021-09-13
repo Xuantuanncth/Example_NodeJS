ca4b943caba19f278e7468ab838c8f6276233a0d
FSOFT.FPT.VN\TuanDX2 <xuantuanncth1@gmail.com>
2021-09-13 22:28:05 +0700

    Update form search

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -8,17 +8,17 @@ console.log(address)
 if (!address) {
     console.log('Please provide an address')
 } else {
-    geocode(address, (error, { latitude, longitude, location }) => {
+    geocode(address, (error,response) => { /// { latitude, longitude, location }
         if (error) {
             return console.log('Error: ', error)
         }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return console.log(error)
-            }
-            console.log('Location: ', location)
-            console.log("Temperature: ", forecastData)
-        })
+        console.log(response)
+        // forecast(latitude, longitude, (error, forecastData) => {
+        //     if (error) {
+        //         return console.log(error)
+        //     }
+        //     console.log('Location: ', location)
+        //     console.log("Temperature: ", forecastData)
+        // })
     })
 }
--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,23 @@
 console.log('This is code test')
+
+fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=DaNang').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.temperature)
+        }
+    })
+})
+
+const watherform = document.querySelector('form')
+watherform.addEventListener('',() =>{
+    
+})
