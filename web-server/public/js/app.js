2b77a4424ce61c5e2165d35ba71e2600c6e94df6
FSOFT.FPT.VN\TuanDX2 <xuantuanncth1@gmail.com>
2021-09-14 07:55:27 +0700

    Complete for base code

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,23 +1,33 @@
-console.log('This is code test')
-
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data)
-    })
-})
-
-fetch('http://localhost:3000/weather?address=DaNang').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.temperature)
-        }
-    })
-})
-
 const watherform = document.querySelector('form')
-watherform.addEventListener('',() =>{
+const search = document.querySelector('input')
+const messageOne =document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+console.log('Client javascrpit loader')
+
+const getWather = (location)=>{
+
+    const url = 'http://localhost:3000/weather?address='+location
+    fetch(url).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error)
+                messageOne.textContent = data.error
+            } else {
+                console.log(data.location)
+                console.log(data.temperature)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.temperature
+            }
+        })
+    })
+}
+
+watherform.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    const location = search.value
+    messageTwo.textContent = ''
+    messageOne.textContent = 'Loading'
+    getWather(location)
     
 })
